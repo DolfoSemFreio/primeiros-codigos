4ed43a4cbf0271c9862f7b0bd85ef7f47ce310cf
dolfolopes <99153377+DolfoSemFreio@users.noreply.github.com>
2022-05-04 16:09:15 -0300

    Create analise_de_dados.py

---
+++ Estudando/Python/Aulas/Repeticoes/While e Break/Exercicios/analise_de_dados.py
@@ -0,0 +1,32 @@
+idade = cont = homem = mulher = 0
+sexo = ''
+
+while True:
+    print('-'*25)
+    print('   CADASTRE UMA PESSOA')
+    print('-'*25)
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+
+    while True:
+        if sexo not in 'MF':
+            sexo = input('Sexo: [M/F]').strip().upper()[0]
+        else:
+            break
+    prox = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while True:
+        if prox not in 'SN':
+                prox = input('Sim ou Nao? ').strip().upper()[0]
+        else:
+            break
+    if idade >= 18:
+        cont += 1
+    if sexo == 'M':
+        homem += 1
+    if sexo == 'F' and idade >= 20:
+        mulher += 1
+    if prox == 'N':
+        break
+print(f'O total de pessoas com mais de 18 anos: {cont}')
+print(f'Ao todo temos {homem} homens cadastrados.')
+print(f'Ao todo {mulher} mulheres com menos de 20 anos')

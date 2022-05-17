cb1b2d20defe2c3f6dc379b7ee737a168696503e
dolfolopes <99153377+DolfoSemFreio@users.noreply.github.com>
2022-05-16 23:02:02 -0300

    Update cadastro_de_jogadores.py

--- Estudando/Python/Aulas/Dicionarios/Exercicios/cadastro_de_jogadores.py
+++ Estudando/Python/Aulas/Dicionarios/Exercicios/cadastro_de_jogadores.py
@@ -1,18 +1,41 @@
+time = []
 gol = []
 dici = {}
-dici['nome'] = input('Nome do jogador: ')
-a = int(input(f'Quantas partidas {dici["nome"]} jogou?'))
-for c in range(0, a):
-    gol.append(int(input(f'Quantos gols na partida {c}? ')))
-dici['gols'] = gol[:]
-dici['total'] = sum(gol)
+while True:
+    dici['nome'] = input('Nome do jogador: ')
+    a = int(input(f'Quantas partidas {dici["nome"]} jogou?'))
+    for c in range(0, a):
+        gol.append(int(input(f'Quantos gols na partida {c+1}? ')))
+    dici['gols'] = gol[:]
+    dici['total'] = sum(gol)
+    time.append(dici.copy())
+    cont = input('Quer continuar? [S/N] ').strip().upper()
+    gol.clear()
+    while cont not in 'NS':
+        cont = input('Tente novamente. Quer continuar? [S/N] ').strip().upper()
+    if cont == 'N':
+        break 
 print('-='*40)
-print(dici)
-print('-='*40)
-for c, n in dici.items():
-    print(f'O campo {c} tem o valor {n}.')
-print('-='*40)
-print(f'O jogador {dici["nome"]} jogou {a} partidas.')
-for i, t in enumerate(dici['gols']):
-    print(f'  => Na partida {i}, fez {t} gols.')
-print(f'foi um total de {dici["total"]}')
+print('cod ', end='')
+for i in dici.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-'*40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')  
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-'*40)
+while True:
+    busca = int(input('Quer mostrar o dado de qual jogador? (999 para interromper) '))
+    if busca == 999:
+        break
+    while busca >= len(time):
+        busca = int(input('ERRO! Jogador nao existe, tente novamente: '))
+    if busca != 999:
+        print(f'>> LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}')
+        for i, g in enumerate(time[busca]["gols"]):
+            print(f'     No jogo {i+1} fez {g} gols.')
+        print('-'*40)
+print('>> VOLTE SEMPRE <<')

8b70372ca931585fa045a52d3f33f6bf6a8a526b
dolfolopes <99153377+DolfoSemFreio@users.noreply.github.com>
2022-05-25 19:45:04 -0300

    Update jogadores.py

--- Estudando/Python/Aulas/Funcoes/Exercicios/jogadores.py
+++ Estudando/Python/Aulas/Funcoes/Exercicios/jogadores.py
@@ -1,15 +1,37 @@
 def ficha(jogador='<desconhecido>', gol = 0):
-    print(f'O jogador {jogador} fez {gol} gol(s) no campeonato.')
+    dic['nome'] = jogador
+    dic['gols'] = gol
+    lista.append(dic.copy())
+
     
+lista = []
+dic = {}
+while True:    
+    joga = input('Nome do jogador: ').strip()
+    g = input('Numero de gols: ')
+    if g.isnumeric():
+        g = int(g)
+    else:
+        g = 0
+    if joga == '':
+        ficha(gol=g)
+    else:
+        ficha(joga, g)
+    cont = input('Quer continuar? [S/N] ').strip().upper()[0]
+    while cont not in 'SN':
+        cont = input('Tente novamente. Quer continuar? [S/N] ').strip().upper()[0]
+    if cont == 'N':
+        break
+print('-='*15)
+print('cod ', end='')
+for c in dic.keys():
+    print(f'{c:<14} ',end='')
+print()
+print('-='*15)
+for n, g in enumerate(lista):
+    print(f'{n:>3} ', end='')
+    for i in g.values():
+        print(f'{i:<15}', end='')
+    print()
+    print('-='*15)
     
-joga = input('Nome do jogador: ').strip()
-g = input('Numero de gols: ')
-if g.isnumeric():
-    g = int(g)
-else:
-    g = 0
-if joga == '':
-    ficha(gol=g)
-else:
-    ficha(joga, g)
-      

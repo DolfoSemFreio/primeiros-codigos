c34b8cb806604575e8c724f2fdbe085d651d874e
dolfolopes <99153377+DolfoSemFreio@users.noreply.github.com>
2022-05-28 23:25:38 -0300

    modulos

---
+++ Estudando/Python/Aulas/Modulos e Pacotes/Aula/modulos_e_pacotes.py
@@ -0,0 +1,6 @@
+import uteis
+
+num = int(input('Digite um valor: '))
+print(f'O fatorial de {num} é {uteis.fatorial(num)}.')
+print(f'O dobre de {num} é {uteis.dobro(num)}.')
+print(f'O triplo de {num} é {uteis.triplo(num)}.')
